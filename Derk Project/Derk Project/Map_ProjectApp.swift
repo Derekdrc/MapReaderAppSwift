0a5d80488bc2889a7cec04b2493307e493866c26
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2022-12-08 12:31:34 -0500

    Added Initial Files Via Upload

---
+++ Derk Project/Derk Project/Map_ProjectApp.swift
@@ -0,0 +1,16 @@
+//
+//  Map_ProjectApp.swift
+//  Map Reader Project
+//
+//
+
+import SwiftUI
+
+@main
+struct Map_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
